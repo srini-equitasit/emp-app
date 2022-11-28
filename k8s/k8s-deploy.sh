516799488d47e2b7f18909804139fd6a7509b40d
srini-equitasit <srini@equitasitinc.com>
2022-11-28 09:41:46 +0530

    Create k8s-deploy.sh

---
+++ k8s/k8s-deploy.sh
@@ -0,0 +1,31 @@
+export AWS_ACCOUNT_ID="337901474843"
+
+export AWS_DEFAULT_REGION="us-east-1"
+
+echo 'login into ECR'
+
+aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
+
+echo 'clean up existing  pods and services'
+
+kubectl delete -f emp-app.secret.config.yml
+
+kubectl delete -f emp-app.config.yml
+
+kubectl delete -f mysql-db-service.deployment.yml
+
+kubectl delete -f emp-service.deployment.yml
+
+kubectl delete -f emp-ui-app-service.deployment.yml
+
+echo 'applying pods and services existing  pods'
+
+kubectl apply -f emp-app.secret.config.yml
+
+kubectl apply -f emp-app.config.yml
+
+kubectl apply -f mysql-db-service.deployment.yml
+
+kubectl apply -f emp-service.deployment.yml
+
+kubectl apply -f emp-ui-app-service.deployment.yml
