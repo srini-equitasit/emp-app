ab2df682d081c7fbbe98afd7bb8146729b166d5b
srini-equitasit <srini@equitasitinc.com>
2022-12-01 15:17:28 +0530

    pod changes

--- k8s/scripts/k8s-deploy.sh
+++ k8s/scripts/k8s-deploy.sh
@@ -2,8 +2,8 @@ kubectl apply -f ../config/emp-app.secret.config.yml
 
 kubectl apply -f ../config/emp-app.config.yml
 
-kubectl apply -f ../db/mysql-8.yml
+kubectl apply -f ../db/mysql-db-service.deployment.yml
 
-# kubectl apply -f emp-service.deployment.yml
+kubectl apply -f emp-service.deployment.yml
 
-# kubectl apply -f emp-ui-app-service.deployment.yml
+kubectl apply -f emp-ui-app-service.deployment.yml
--- k8s/scripts/k8s-un-deploy.sh
+++ k8s/scripts/k8s-un-deploy.sh
@@ -1,11 +1,11 @@
 echo 'clean up existing  pods and services'
 
-kubectl delete -f ../config/emp-app.secret.config.yml
-
-kubectl delete -f ../config/emp-app.config.yml
-
-kubectl delete -f ../db/mysql-8.yml
+kubectl delete -f ../emp-ui-app-service.deployment.yml
 
 kubectl delete -f ../emp-service.deployment.yml
 
-kubectl delete -f ../emp-ui-app-service.deployment.yml
+kubectl delete -f ../db/mysql-db-service.deployment.yml
+
+kubectl delete -f ../config/emp-app.secret.config.yml
+
+kubectl delete -f ../config/emp-app.config.yml
