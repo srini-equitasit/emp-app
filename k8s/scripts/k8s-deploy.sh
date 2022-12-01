a43d2bd558d656df942764a20e87b88f7d147ebb
srini-equitasit <srini@equitasitinc.com>
2022-12-01 15:39:54 +0530

    Update k8s-deploy.sh

--- k8s/scripts/k8s-deploy.sh
+++ k8s/scripts/k8s-deploy.sh
@@ -4,6 +4,6 @@ kubectl apply -f ../config/emp-app.config.yml
 
 kubectl apply -f ../db/mysql-db-service.deployment.yml
 
-kubectl apply -f emp-service.deployment.yml
+kubectl apply -f ../emp-service.deployment.yml
 
-kubectl apply -f emp-ui-app-service.deployment.yml
+kubectl apply -f ../emp-ui-app-service.deployment.yml
